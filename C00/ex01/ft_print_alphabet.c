f2d9755b22f0c61e75e50625c4aa770d7c375ca8
Queeeesu <112662401+Queeeesu@users.noreply.github.com>
2022-09-02 14:01:45 +0100

    Add files via upload

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akassou <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 20:32:37 by akassou           #+#    #+#             */
+/*   Updated: 2022/08/30 20:39:51 by akassou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		ft_putchar(alphabet);
+		alphabet++;
+	}
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akassou <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 18:27:55 by akassou           #+#    #+#             */
+/*   Updated: 2022/08/29 19:43:37 by akassou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		ft_putchar(alphabet);
+		alphabet--;
+	}
+}
